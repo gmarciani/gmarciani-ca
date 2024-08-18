b697c1889fca49c55cd20b6b0b7e6649e019c787
Giacomo Marciani <mgiacomo@amazon.com>
2024-08-19 00:19:52 +0200

    [Tools] Add script to setup intermediate-ca.

--- tools/build-intermediate-ca-certificate.sh
+++ tools/build-intermediate-ca-certificate.sh
@@ -1,41 +1,72 @@
 #!/usr/bin/env bash
+set -e
 
 # Usage: build-intermediate-ca-certificate.sh
 
-CURRENT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-PROJECT_PATH="$CURRENT_PATH/.."
+PROJECT_PATH="$( cd -- "$(dirname "$0")/.." >/dev/null 2>&1 ; pwd -P )"
+
+# Main Folders
 ROOT_CA_PATH="$PROJECT_PATH/root-ca"
+ROOT_CA_CERTS_PATH="$ROOT_CA_PATH/certs"
 INTERMEDIATE_CA_PATH="$PROJECT_PATH/intermediate-ca"
 
-cd "$INTERMEDIATE_CA_PATH" || exit 1
+# OpenSSL Configuration
+ROOT_CA_CONFIG="$ROOT_CA_PATH/openssl.cfg"
+INTERMEDIATE_CA_CONFIG="$INTERMEDIATE_CA_PATH/openssl.cfg"
 
-mkdir certs crl csr newcerts private
-chmod 700 private
-touch index.txt
-echo 1000 > serial
-echo 1000 > crlnumber
-openssl genrsa -out private/intermediate.key.pem 2048
-chmod 400 private/intermediate.key.pem
+# Output Directories
+INTERMEDIATE_CA_PRIVATE_PATH="$INTERMEDIATE_CA_PATH/private"
+INTERMEDIATE_CA_CERTS_PATH="$INTERMEDIATE_CA_PATH/certs"
+INTERMEDIATE_CA_NEWCERTS_PATH="$INTERMEDIATE_CA_PATH/newcerts"
+INTERMEDIATE_CA_CRL_PATH="$INTERMEDIATE_CA_PATH/crl"
+INTERMEDIATE_CA_CSR_PATH="$INTERMEDIATE_CA_PATH/csr"
 
+# Files
+INTERMEDIATE_CA_PRIVATE_KEY="$INTERMEDIATE_CA_PRIVATE_PATH/intermediate.key.pem"
+INTERMEDIATE_CA_CERT="$INTERMEDIATE_CA_CERTS_PATH/intermediate.cert.pem"
+INTERMEDIATE_CA_CSR="$INTERMEDIATE_CA_CSR_PATH/intermediate.csr.pem"
+INTERMEDIATE_CA_INDEX="$INTERMEDIATE_CA_PATH/index.txt"
+INTERMEDIATE_CA_SERIAL="$INTERMEDIATE_CA_PATH/serial"
+INTERMEDIATE_CA_CRLNUMBER="$INTERMEDIATE_CA_PATH/crlnumber"
+ROOT_CA_CERT="$ROOT_CA_CERTS_PATH/ca.cert.pem"
+CA_CHAIN_CERT="$INTERMEDIATE_CA_CERTS_PATH/ca-chain.cert.pem"
+
+# Initialize folders and files
+mkdir -p "$INTERMEDIATE_CA_PRIVATE_PATH"
+mkdir -p "$INTERMEDIATE_CA_CERTS_PATH"
+mkdir -p "$INTERMEDIATE_CA_NEWCERTS_PATH"
+mkdir -p "$INTERMEDIATE_CA_CRL_PATH"
+mkdir -p "$INTERMEDIATE_CA_CSR_PATH"
+chmod 700 "$INTERMEDIATE_CA_PRIVATE_PATH"
+touch "$INTERMEDIATE_CA_INDEX"
+echo 1000 > "$INTERMEDIATE_CA_SERIAL"
+echo 1000 > "$INTERMEDIATE_CA_CRLNUMBER"
+
+# Create Private Key for the Intermediate CA
+openssl genrsa -out "$INTERMEDIATE_CA_PRIVATE_KEY" 2048
+chmod 400 "$INTERMEDIATE_CA_PRIVATE_KEY"
+
+# Create Certificate Signing Request
 cd "$ROOT_CA_PATH" || exit 1
+openssl req -config "$INTERMEDIATE_CA_CONFIG" \
+      -new -sha256 \
+      -key "$INTERMEDIATE_CA_PRIVATE_KEY" \
+      -out "$INTERMEDIATE_CA_CSR"
 
-openssl req -config "$INTERMEDIATE_CA_PATH/openssl.cfg" -new -sha256 \
-      -key "$INTERMEDIATE_CA_PATH/private/intermediate.key.pem" \
-      -out "$INTERMEDIATE_CA_PATH/csr/intermediate.csr.pem"
-
+# Create Certificate for the Intermediate CA
 cd "$ROOT_CA_PATH" || exit 1
-openssl ca -config openssl.cfg -extensions v3_intermediate_ca \
+openssl ca -config "$ROOT_CA_CONFIG" \
+      -extensions v3_intermediate_ca \
       -days 3650 -notext -md sha256 \
-      -in "$INTERMEDIATE_CA_PATH/csr/intermediate.csr.pem" \
-      -out "$INTERMEDIATE_CA_PATH/certs/intermediate.cert.pem"
-chmod 444 "$INTERMEDIATE_CA_PATH/certs/intermediate.cert.pem"
+      -batch \
+      -in "$INTERMEDIATE_CA_CSR" \
+      -out "$INTERMEDIATE_CA_CERT"
+chmod 444 "$INTERMEDIATE_CA_CERT"
 
-openssl x509 -noout -text \
-      -in "$INTERMEDIATE_CA_PATH/certs/intermediate.cert.pem"
+# Checks
+openssl x509 -noout -text -in "$INTERMEDIATE_CA_CERT"
+openssl verify -CAfile "$ROOT_CA_CERT" "$INTERMEDIATE_CA_CERT"
 
-openssl verify -CAfile "$ROOT_CA_PATH/certs/ca.cert.pem" \
-      "$INTERMEDIATE_CA_PATH/certs/intermediate.cert.pem"
-
-cat "$INTERMEDIATE_CA_PATH/certs/intermediate.cert.pem" \
-      "$ROOT_CA_PATH/certs/ca.cert.pem" > "$INTERMEDIATE_CA_PATH/certs/gmarciani-ca-chain.cert.pem"
-chmod 444 "$INTERMEDIATE_CA_PATH/certs/gmarciani-ca-chain.cert.pem"
+# CA Chain
+cat "$INTERMEDIATE_CA_CERT" "$ROOT_CA_CERT" > "$CA_CHAIN_CERT"
+chmod 444 "$CA_CHAIN_CERT"
